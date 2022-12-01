e9119d8a712e7a6e7372b0c103dbc8ac6e064378
yakovgavryushov <95687203+yakovgavryushov@users.noreply.github.com>
2022-12-01 18:39:04 +0300

    proj added

---
+++ main.js
@@ -0,0 +1,144 @@
+let addBtn = document.getElementById("addBtn");
+let itemsList = document.querySelector("#itemsList");
+let addItemVal = document.getElementById("addItem").value;
+let pages = document.getElementById("pages");
+let unfinished = document.getElementById("undoneNum");
+let unfinishedNum = 0;
+unfinished.innerHTML = unfinishedNum;
+let total = document.getElementById("totalNum");
+let totalNum = 0;
+total.innerHTML = totalNum;
+let checkedElement = document.getElementById("checkedNum");
+let checkedNum = 0;
+checkedElement.innerHTML = checkedNum;
+/// / / / // / // / // / // / // / // / // / // / // / // / // / // / ///
+
+let itemCounter = [];
+let pageCounter = 1;
+
+// add New Item
+function addNewItem() {
+  checkAll.checked = false;
+  unfinishedNum++;
+  unfinished.innerHTML = unfinishedNum;
+  totalNum++;
+  total.innerHTML = totalNum;
+  //---------------------------------------------------------fix required
+  if (itemCounter.length >= 8 && itemCounter.length % 8 === 0) {
+    pageCounter++;
+    let newPage = document.createElement("a");
+    newPage.id = "page" + `${pageCounter}`;
+    newPage.innerHTML = `${pageCounter}`;
+    pages.append(newPage);
+  }
+  //------------------------------------
+  let value = document.getElementById("addItem").value;
+  let newItem = document.createElement(`li`);
+  newItem.innerHTML = `${value}`;
+
+  itemsList.append(newItem);
+  itemCounter.push(newItem.innerHTML);
+
+  //closebtn
+  let closeBtn = document.createElement("a");
+  let thisArrItem = (closeBtn.className = // className
+    itemCounter.at(-1) + " item" + `${itemCounter.length - 1}`); // className
+  closeBtn.innerHTML = "&#10006";
+
+  closeBtn.addEventListener("click", () => {
+    newItem.remove();
+    unfinishedNum--;
+    unfinished.innerHTML = unfinishedNum;
+    totalNum--;
+    total.innerHTML = totalNum;
+    if (checkBtn.checked) {
+      checkedNum--;
+      checkedElement.innerHTML = checkedNum;
+      unfinishedNum++;
+      unfinished.innerHTML = unfinishedNum;
+    }
+    if (itemCounter.length >= 9 && itemCounter.length % 9 === 0) {
+      let neededCounter = document.getElementById(`page${pageCounter}`);
+      neededCounter.remove();
+      pageCounter--;
+    }
+    itemCounter.shift(`${thisArrItem + 1}`);
+  });
+  newItem.append(closeBtn);
+
+  // checkbtn
+  let checkBtn = document.createElement("input");
+  checkBtn.type = "checkbox";
+  checkBtn.className = "checkbox";
+  newItem.prepend(checkBtn);
+  checkBtn.addEventListener("change", function () {
+    if (this.checked) {
+      unfinishedNum--;
+      unfinished.innerHTML = unfinishedNum;
+      checkedNum++;
+      checkedElement.innerHTML = checkedNum;
+    } else {
+      unfinishedNum++;
+      unfinished.innerHTML = unfinishedNum;
+      checkedNum--;
+      checkedElement.innerHTML = checkedNum;
+    }
+  });
+}
+
+// check all btn
+let checkAll = document.getElementById("checkAll");
+checkAll.checked = false;
+checkAll.addEventListener("change", () => {
+  let length = itemsList.children.length;
+  if (checkAll.checked) {
+    for (let i = 0; i < length; i++) {
+      if (itemsList.children[i].children[0].checked === false) {
+        itemsList.children[i].children[0].checked = true;
+        unfinishedNum--;
+        unfinished.innerHTML = unfinishedNum;
+        checkedNum++;
+        checkedElement.innerHTML = checkedNum;
+      }
+    }
+  } else {
+    for (let i = 0; i < length; i++) {
+      itemsList.children[i].children[0].checked = false;
+      unfinishedNum++;
+      unfinished.innerHTML = unfinishedNum;
+      checkedNum--;
+      checkedElement.innerHTML = checkedNum;
+    }
+  }
+});
+// delete checked
+//-----------------------------------------------------------fix required
+let delChecked = document.getElementById("delChecked");
+delChecked.addEventListener("click", () => {
+  let length = itemsList.children.length;
+  for (let i = 0; i < length; i++) {
+    if (itemsList.children[i].children[0].checked) {
+      itemsList.children[i].remove();
+      totalNum--;
+      total.innerHTML = totalNum;
+      checkedNum--;
+      checkedElement.innerHTML = checkedNum;
+
+      /*       if (itemCounter.length >= 9 && itemCounter.length % 9 === 0) {
+        let neededCounter = document.getElementById(`page${pageCounter}`);
+        neededCounter.remove();
+        pageCounter--;
+      }
+      itemCounter.shift(`${thisArrItem + 1}`); */
+    }
+  }
+});
+//----------------------------------------------------------------------
+/*
+/ // needed:
+
+/ // switching pages;
+
+/ // fix delChecked 1/2 error
+/ // fix delChecked page counter not decreasing
+*/
